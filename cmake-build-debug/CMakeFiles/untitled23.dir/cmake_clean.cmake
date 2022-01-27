4d8973527e93c854d17140aa26ddf206d5502a59
gavin <audesandrine6@gmail.com>
2022-01-27 15:13:54 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/untitled23.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled23.dir/main.c.o"
+  "untitled23"
+  "untitled23.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
