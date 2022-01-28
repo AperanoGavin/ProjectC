b91f30f9b638feba22312a9742fae99a26fcd4c5
gavin <audesandrine6@gmail.com>
2022-01-28 01:29:12 +0100

    initialization login

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include<string.h>
 #include <unistd.h>
 
+#define ENTER 13
+#define TAB 9
+#define BCKSPC 8
+
 struct user{
     char fullName[50];
     char email[50];
@@ -10,6 +14,7 @@ struct user{
     char username[50];
     char contact[50];
     char age[50];
+    char adresse[200];
 };
 
 void takeinput(char ch[50]){
@@ -26,18 +31,40 @@ void generateUsername(char email[50], char username[50]){
 
 }
 
+void takepassword(char password[50]){
+    int i;
+    char ch;
+    while(1){
+        ch = getch();
+        if (ch == ENTER || ch == TAB){
+            password[i]='\0';
+            break;
+        } else if(ch == BCKSPC){
+            if(i<0){
+                i--;
+                printf("\b \b");
+            }
+
+        }else{
+            password[i++] = ch;
+            printf("* \b");
+        }
+    }
+}
+
 
 int main(int argc, char **argv) {
 
 
     int opt;
+    int opt2;
     struct user user;
 
     printf("\n\t\t\t\t--------------------Welcom to Doctodog  --------------------");
     printf("\nChoose your operation ");
     printf("\n1.Signup");
     printf("\n2.Login");
-    printf("\n2.Exit");
+    printf("\n3.Exit");
 
     printf("\n\nYour choice:\t");
     scanf("%d",&opt);
@@ -45,16 +72,56 @@ int main(int argc, char **argv) {
 
     switch (opt) {
         case 1:
-            printf("\nEnter full name of your dog:\t");
-            takeinput(user.fullName);
-            printf("\nEnter your email :\t");
-            takeinput(user.email);
-            printf("\nEnter  age of your dog :\t");
-            takeinput(user.age);
-            printf("\nEnter your contact :\t");
-            takeinput(user.contact);
-            printf("\nEnter your password :\t");
-            takeinput(user.password);
+
+            printf("\n\t\t\t\t--------------------Signup --------------------");
+            printf("\nChoose your operation ");
+            printf("\n1.Signup for Dog");
+            printf("\n2.Signup for Doctor");
+            printf("\n3.Exit");
+
+            printf("\n\nYour choice:\t");
+            scanf("%d",&opt2);
+            fgetc(stdin);
+
+            switch(opt2){
+                case 1:
+                    printf("\nEnter full name of your dog:\t");
+                    takeinput(user.fullName);
+                    printf("Enter your email :\t");
+                    takeinput(user.email);
+                    printf("Enter  age of your dog :\t");
+                    takeinput(user.age);
+                    printf("Enter your contact :\t");
+                    takeinput(user.contact);
+                    printf("Enter your adresse :\t");
+                    takeinput(user.adresse);
+                    printf("Enter your password :\t");
+                    takepassword(user.password);
+                    break;
+                case 2:
+
+                    printf("Enter full name:\t");
+                    takeinput(user.fullName);
+                    printf("Enter your email :\t");
+                    takeinput(user.email);
+                    printf("Enter  age :\t");
+                    takeinput(user.age);
+                    printf("Enter your contact :\t");
+                    takeinput(user.contact);
+                    printf("Enter your adresse :\t");
+                    takeinput(user.adresse);
+                    printf("Enter your password :\t");
+                    takepassword(user.password);
+                    break;
+
+                case 3:
+                    break;
+            }
+
+        case 2:
+            break;
+        case 3:
+            break;
 
 
     }
