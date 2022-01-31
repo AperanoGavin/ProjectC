f923a86b0682449e338eee8bb62c54de696ba1db
gavin <audesandrine6@gmail.com>
2022-01-31 15:41:35 +0100

    add sql connect and advancement on login

--- main.c
+++ main.c
@@ -1,13 +1,36 @@
 #include "./conio for mac and windows/myconio_mac.h"
 #include<stdio.h>
 #include<string.h>
+#include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
+#include "./include/mysql.h"
 
+void main(){
+    MYSQL *con = NULL;
+    printf("initializing mysql ..\n");
+    con = mysql_init(NULL);
+    if(NULL == con){
+        printf("Failed to initialize mysql ...\n");
+        printf("Problem: %s\n",mysql_error(con));
+        return;
+    }
+    printf("attempting to connect \n");
+    if(NULL  == mysql_real_connect(con,"localhost","root","",NULL, 0, NULL, 0)){
+        printf("Failed to connect mysql ...\n");
+        printf("Problem: %s\n",mysql_error(con));
+        return;
+
+    }
+    printf("connected to  mysql ...\n");
+    mysql_error(con);
+}
 
 
 #define ENTER 13
 #define TAB 9
 #define BCKSPC 8
+
 
 
 struct user{
